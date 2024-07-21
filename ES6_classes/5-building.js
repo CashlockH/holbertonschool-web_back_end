27382c2ce6ce3a126b09544a7c6b07777913c10c
CashlockH <ilyashesenli48@gmail.com>
2024-07-21 20:06:34 +0400

    initialize in constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,12 +1,13 @@
 export default class Building{
     constructor(sqft) {
         this._sqft = sqft
-    }
-
-    get sqft(){
-        return this._sqft
+        this.evacuationWarningMessage()
     }
     evacuationWarningMessage(){
-        throw new Error("Class extending Building must override evacuationWarningMessage")
+        if (this.constructor != Building)
+            throw new Error("Class extending Building must override evacuationWarningMessage")
+    }
+    get sqft(){
+        return this._sqft
     }
 }
