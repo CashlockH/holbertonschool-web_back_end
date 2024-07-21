51b67792f73f2bac9928cd841ae67249ecbab495
CashlockH <ilyashesenli48@gmail.com>
2024-07-21 18:13:01 +0400

    eslint

--- ES6_classes/eslint.config.mjs
+++ ES6_classes/eslint.config.mjs
@@ -1,13 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
-
+// eslint.config.js
+import js from "@eslint/js";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
+    js.configs.recommended,
+
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        }
+    }
 ];
