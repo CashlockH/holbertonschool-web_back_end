a9b78fb9ec5313a9cf98e2a36f22b3d05f2cec46
CashlockH <ilyashesenli48@gmail.com>
2024-07-21 20:32:22 +0400

    to primitive

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,15 @@
+export default class HolbertonClass{
+    constructor(size, location) {
+        this._size = size,
+        this._location = location
+    }
+
+    [Symbol.toPrimitive](x){
+        if (x === 'string'){
+            return this._location
+        }
+        else if(x === 'number'){
+            return this._size
+        }
+    }
+}
