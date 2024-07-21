a7055188835a6cc87ae1dc3d529dfb15606114bd
CashlockH <ilyashesenli48@gmail.com>
2024-07-21 18:24:30 +0400

    without new

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 import ClassRoom  from "./0-classroom";
 
 function initializeRooms(){
-    return new Array(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34))
+    new array = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)]
 }
