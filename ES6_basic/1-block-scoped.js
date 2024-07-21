7c4dd0424a9836efd1420b43b89397a8ef74c097
CashlockH <ilyashesenli48@gmail.com>
2024-07-21 13:08:38 +0400

    hoisting

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-    task = false;
-    task2 = true;
-  
     if (trueOrFalse) {
       var task = true;
       var task2 = false;
     }
-  
+    
+    task = false;
+    task2 = true;
+    
     return [task, task2];
   }
+
+  console.log(taskBlock(true));
+  console.log(taskBlock(false));
