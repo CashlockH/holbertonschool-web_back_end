51bf4d9b9577288a92abe07a20ffb6232a835fa8
CashlockH <ilyashesenli48@gmail.com>
2024-07-21 13:40:14 +0400

    ()

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,12 +1,8 @@
 export default function getNeighborhoodsList() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
   
-    this.addNeighborhood = newNeighborhood => {
+    this.addNeighborhood = (newNeighborhood) => {
       this.sanFranciscoNeighborhoods.push(newNeighborhood);
       return this.sanFranciscoNeighborhoods;
     };
   }
-
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
