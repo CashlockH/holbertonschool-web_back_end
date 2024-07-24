ef545cd9da60656e17dc9c593544dd4136077c40
CashlockH <ilyashesenli48@gmail.com>
2024-07-24 11:33:52 +0400

    null

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,12 @@
+import getListStudents from "./0-get_list_students.js";
+
 export default function getListStudentIds(array_obj){
-    if (typeof array_obj != 'object'){
+    if (typeof array_obj != 'object' || array_obj == null){
         return []
     }
     else{
         return array_obj.map((obj) => obj['id'])
     }
 }
+
+console.log(getListStudentIds(null));
