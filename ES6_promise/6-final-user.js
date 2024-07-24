281a3bdae78d616ba6eefdb69c4ac18821d2a5d3
CashlockH <ilyashesenli48@gmail.com>
2024-07-24 22:36:32 +0400

    return

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,7 +5,10 @@ export default function handleProfileSignup(firstName, lastName, filename){
     return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then(
         (response) => {
-            return response
+            return {
+            status: response[0].status,
+            value: response[0].value
         }
+    }
     )
 }
