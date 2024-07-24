18fd480a684efe481f1ab342b8cc24233cca4a3b
CashlockH <ilyashesenli48@gmail.com>
2024-07-24 11:47:04 +0400

    no startString

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set_obj, startString){
     let text = ""
     for (const element of set_obj)
     {
-        if (element.startsWith(startString) && startString)
+        if (startString && element.startsWith(startString))
         {
             if (text)
                 text = text.concat("-", element.slice(startString.length))
