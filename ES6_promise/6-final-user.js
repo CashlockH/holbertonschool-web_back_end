cb4dba0794283b365c7208b2ae680eb50231edca
CashlockH <ilyashesenli48@gmail.com>
2024-07-24 22:39:22 +0400

    reject part

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,10 +5,14 @@ export default function handleProfileSignup(firstName, lastName, filename){
     return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then(
         (response) => {
-            return {
+            return [{
             status: response[0].status,
             value: response[0].value
-        }
+        },
+        {
+            status: response[1].status,
+            value: response[1].reason
+        }]
     }
     )
 }
