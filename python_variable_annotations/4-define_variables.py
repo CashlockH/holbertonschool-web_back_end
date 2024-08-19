9e8c5855f4b8ab75aa13c3a4b1f0e41fa25b221a
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 22:37:04 +0400

    pycodestyle

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 """some variables with its values and annotations"""
-a: int = 1; pi: float = 3.14; i_understand_annotations: bool = True; school: str = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
