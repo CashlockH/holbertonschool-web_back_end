4ce489e9bc4139cf6c8d7e10678fabff228a7532
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 22:50:44 +0400

    list of union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """sum_mixed_list funciton with annotations"""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """takes list of integers and floats returns their sum as float"""
     sum = 0
     for element in mxd_lst:
