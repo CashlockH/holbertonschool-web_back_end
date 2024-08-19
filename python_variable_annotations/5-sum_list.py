00950491ec304748125bc62d5a2e8fad94194da4
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 22:45:29 +0400

    pycodestyle

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """sum_list funciton with annotations"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """takes list of floats as an input and returns their sum"""
     sum = 0
