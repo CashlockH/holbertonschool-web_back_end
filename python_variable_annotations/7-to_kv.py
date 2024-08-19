72c00bcada0cca548a1bd608f42f4e222b37f514
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 23:38:30 +0400

    returning a tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes string as the first input, and float or int as the second
     input then returns tupel containing them"""
-    return tuple(k, v)
+    return (k, v)
