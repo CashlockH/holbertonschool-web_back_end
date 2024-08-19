8629797c615a5d6656f4bbab9c194a8c564694fa
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 23:06:57 +0400

    to_kv funciton

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""to_kv funciton with annotations"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """takes string as the first input, and float or int as the second
+    input then returns tupel containing them"""
+    return tuple(k, v)
