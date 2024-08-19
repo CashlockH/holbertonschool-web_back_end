d19bcfe3961043eacffd443048b94ce136b77656
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 23:17:42 +0400

    return tuple element is float

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """takes string as the first input, and float or int as the second
     input then returns tupel containing them"""
     return tuple(k, v)
