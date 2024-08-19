0728b891beb566fbfc70656c63dff2877912522a
CashlockH <ilyashesenli48@gmail.com>
2024-08-19 22:16:42 +0400

    concatenation function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Concatenation function with annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two strings and return concatenated string"""
+    return str1 + str2
