63697ecafffb0cd42b7edcd4c1804db1db69afcc
CashlockH <ilyashesenli48@gmail.com>
2024-08-20 20:25:58 +0400

    wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """wait_n function module"""
-wait_random = __import__('0-basic_async_syntax').wait_random
 from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    
+    """calls max_delay function n times and return its values sorted"""
+    return_list = []
+    for i in range(n):
+        a = asyncio.create_task(wait_random(max_delay))
+        return_list.append(a)
+    return await asyncio.gather(wait_random(5), wait_random(5))
