fd9376d579d12b00b452fcd01eb7724fa182bb90
CashlockH <ilyashesenli48@gmail.com>
2024-08-21 11:24:18 +0400

    async_generator function

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async_generator funciton module"""
+import asyncio
+from typing import AsyncGenerator
+import random
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Loop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10."""
+    for i in range(10):
+        yield random.uniform(0, 10)
