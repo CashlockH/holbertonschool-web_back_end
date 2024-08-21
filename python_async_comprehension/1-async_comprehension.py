63803a72e4fdb264555797eb22ff806d359406bf
CashlockH <ilyashesenli48@gmail.com>
2024-08-21 12:47:38 +0400

    async_comprehension funciton

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""async_comprehension funciton module."""
+import asyncio
+from typing import List
+async_generator  = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """do asynchronous comprehension over async_generator,
+    collect its values and return as a list"""
+    return_list = [i async for i in async_generator()]
+    return return_list
