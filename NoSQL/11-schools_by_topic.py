7868b5fb8c00831fe8923aaadf96312943b09ef6
CashlockH <ilyashesenli48@gmail.com>
2024-08-22 22:20:54 +0400

    attribute name corrected

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Finds documents that have specified topic"""
-    return mongo_collection.find({"topic": topic})
+    return mongo_collection.find({"topics": topic})
