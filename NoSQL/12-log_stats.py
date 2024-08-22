5aafa846074c023868a13f528921199080c17d48
CashlockH <ilyashesenli48@gmail.com>
2024-08-22 23:24:16 +0400

    output correction and clean code

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,34 +2,36 @@
 """a Python script that provides some stats about
 Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
-client = MongoClient()
-db = client.logs
-collection = db.nginx
-
 
 def get_stats():
-    """gets all documents and display it in valid format"""
-    documents = collection.find()
+    """Gets all documents and displays them in a valid format"""
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
+
     methods = {
         'GET': 0,
         'POST': 0,
         'PUT': 0,
         'PATCH': 0,
-        'DELETE': 0}
+        'DELETE': 0
+    }
     status_check = 0
-    logs = 0
-    for document in documents:
-        logs += 1
-        if document['method'] in methods:
-            methods[document['method']] += 1
-        if document['path'] == '/status' and document['method'] == 'GET':
+    logs = collection.count_documents({})
+
+    for document in collection.find():
+        method = document.get('method')
+        path = document.get('path')
+        if method in methods:
+            methods[method] += 1
+        if method == 'GET' and path == '/status':
             status_check += 1
-    print("{}".format(logs))
+
+    print("{} logs".format(logs))
     print("Methods:")
     for key, value in methods.items():
         print("\tmethod {}: {}".format(key, value))
     print("{} status check".format(status_check))
-
 
 if __name__ == '__main__':
     get_stats()
