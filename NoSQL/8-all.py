313c559d80ffa5e09fe3bc0b0cac1fd21b802f55
CashlockH <ilyashesenli48@gmail.com>
2024-08-22 21:36:24 +0400

    pycodestyle and documentation

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""list_all funciton module"""
+
+
 def list_all(mongo_collection):
-    list = [element for element in mongo_collection.find()]
+    """Lists all documents in collection"""
+    list = [element for element
+            in mongo_collection.find() if mongo_collection.find()]
     return list
