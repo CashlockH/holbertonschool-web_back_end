7cb5d6cba6858ca59eb397f6eaa98d082eacc2c4
CashlockH <ilyashesenli48@gmail.com>
2024-08-25 21:11:37 +0400

    10 times

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,12 +3,11 @@
 import asyncio
 from typing import AsyncGenerator
 import random
-import time
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10."""
-    for i in range(10):
+    for i in range(9):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
