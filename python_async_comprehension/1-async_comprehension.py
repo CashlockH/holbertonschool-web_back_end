c38d1adf12c19108db035c4dffbc52596844251c
CashlockH <ilyashesenli48@gmail.com>
2024-08-25 21:18:01 +0400

    pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,7 @@
 """async_comprehension funciton module."""
 import asyncio
 from typing import List
-async_generator  = __import__('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
