014783ca903529342290699975a22bec87db01ba
CashlockH <ilyashesenli48@gmail.com>
2024-09-22 19:02:16 +0400

    try

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes/index');
+
+const app = express();
+const PORT = 1245;
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
