d0503c2d82d85f0a2583c1955938eab92692e6c4
CashlockH <ilyashesenli48@gmail.com>
2024-09-22 17:21:15 +0400

    server with express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello Holberton School!');
+});
+port = 1245
+app.listen(port, () => {
+    console.log(`Server running at http://127.0.0.1:${port}/`);
+});
