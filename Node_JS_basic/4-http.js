ca7a859048ab2bd800d564b4155359396acc72af
CashlockH <ilyashesenli48@gmail.com>
2024-09-22 15:48:27 +0400

    output

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('node:http');
 const app = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!\n');
+    res.end('Hello Holberton School!');
 });
 app.listen(1245, () => {
     console.log('Server running at http://127.0.0.1:1245/');
